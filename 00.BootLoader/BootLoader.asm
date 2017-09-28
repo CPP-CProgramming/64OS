3629e9e80e3e39f764ba6812617c102a6b0b4a4e
neogeoss <neogeo-s@hanmail.net>
2017-09-28 23:02:07 +0900

    Works fine!

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -14,11 +14,8 @@ jmp 0x07C0:START    ; CS 세그먼트 레지스터에 0x07C0을 복사하면서, START 레이블로
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;   MINT64 OS에 관련된 환경 설정 값
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-TOTALSECTORCOUNT:   dw  0x02    ; 부트 로더를 제외한 MINT64 OS 이미지의 크기
-                                ; 최대 1146 섹터(0x8F400byte)까지 가능
-KERNEL32SECTORCOUNT: dw 0x02    ; 보호 모드 커널의 총 섹터 수
-BOOTSTRAPPROCESSOR: db 0x01     ; Bootstrap Processor인지 여부
-STARTGRAPHICMODE:   db 0x01     ; 그래픽 모드로 시작하는지 여부
+TOTALSECTORCOUNT:   dw  0x01    ; 부트 로더를 제외한 MINT64 OS 이미지의 크기
+                                ; 최대 1152 섹터(0x90000byte)까지 가능
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;   코드 영역
@@ -35,13 +32,11 @@ START:
     mov sp, 0xFFFE  ; SP 레지스터의 어드레스를 0xFFFE로 설정
     mov bp, 0xFFFE  ; BP 레지스터의 어드레스를 0xFFFE로 설정
 
-    mov byte[ BOOTDRIVE ], dl       ; 부팅한 드라이브의 번호를 메모리에 저장
-
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; 화면을 모두 지우고, 속성값을 녹색으로 설정
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     mov si,    0                    ; SI 레지스터(문자열 원본 인덱스 레지스터)를 초기화
-    
+        
 .SCREENCLEARLOOP:                   ; 화면을 지우는 루프
     mov byte [ es: si ], 0          ; 비디오 메모리의 문자가 위치하는 어드레스에
                                     ; 0을 복사하여 문자를 삭제
@@ -86,25 +81,10 @@ RESETDISK:                          ; 디스크를 리셋하는 코드의 시작
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; 서비스 번호 0, 드라이브 번호(0=Floppy)
     mov ax, 0
-    mov dl, byte [ BOOTDRIVE ]
+    mov dl, 0              
     int 0x13     
     ; 에러가 발생하면 에러 처리로 이동
     jc  HANDLEDISKERROR
-
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; 디스크 파라미터를 읽음
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    mov     ah, 0x08                    ; BIOS 서비스 번호 8(Read Disk Parameters)
-    mov     dl, byte [ BOOTDRIVE ]      ; 읽을 드라이브 번호(USB Flash Drive) 설정
-    int     0x13                        ; 인터럽트 서비스 수행
-    jc      HANDLEDISKERROR             ; 에러가 발생했다면 HANDLEDISKERROR로 이동
-
-    mov     byte [ LASTHEAD ], dh       ; 헤드 정보를 메모리에 저장
-    mov     al, cl                      ; 섹터와 트랙 정보를 AL 레지스터에 저장
-    and     al, 0x3f                    ; 섹터 정보(하위 6 비트)를 추출하여
-                                        ; AL 레지스터에 저장
-    mov     byte [ LASTSECTOR ], al     ; 섹터 정보를 메모리에 저장
-    mov     byte [ LASTTRACK ], ch      ; 트랙 정보 중에서 하위 8 비트를 메모리에 저장
         
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; 디스크에서 섹터를 읽음
@@ -115,11 +95,9 @@ RESETDISK:                          ; 디스크를 리셋하는 코드의 시작
     mov es, si                      ; ES 세그먼트 레지스터에 값 설정
     mov bx, 0x0000                  ; BX 레지스터에 0x0000을 설정하여 복사할 
                                     ; 어드레스를 0x1000:0000(0x10000)으로 최종 설정
-        
-    ;mov di, word [ TOTALSECTORCOUNT ] ; 복사할 OS 이미지의 섹터 수를 DI 레지스터에 설정
-    mov di, 1146                    ; OS 이미지 뒤에 있는 패키지 파일을 로딩하려고
-                                    ; 573Kbyte(1146 섹터)까지 읽도록 개수를 설정
-        
+
+    mov di, word [ TOTALSECTORCOUNT ] ; 복사할 OS 이미지의 섹터 수를 DI 레지스터에 설정
+
 READDATA:                           ; 디스크를 읽는 코드의 시작
     ; 모든 섹터를 다 읽었는지 확인
     cmp di, 0               ; 복사할 OS 이미지의 섹터 수를 0과 비교
@@ -134,7 +112,7 @@ READDATA:                           ; 디스크를 읽는 코드의 시작
     mov ch, byte [ TRACKNUMBER ]        ; 읽을 트랙 번호 설정
     mov cl, byte [ SECTORNUMBER ]       ; 읽을 섹터 번호 설정
     mov dh, byte [ HEADNUMBER ]         ; 읽을 헤드 번호 설정
-    mov dl, byte [ BOOTDRIVE ]          ; 읽을 드라이브 번호(0=Floppy) 설정
+    mov dl, 0x00                        ; 읽을 드라이브 번호(0=Floppy) 설정
     int 0x13                            ; 인터럽트 서비스 수행
     jc HANDLEDISKERROR                  ; 에러가 발생했다면 HANDLEDISKERROR로 이동
     
@@ -150,26 +128,22 @@ READDATA:                           ; 디스크를 읽는 코드의 시작
     mov al, byte [ SECTORNUMBER ]       ; 섹터 번호를 AL 레지스터에 설정
     add al, 0x01                        ; 섹터 번호를 1 증가
     mov byte [ SECTORNUMBER ], al       ; 증가시킨 섹터 번호를 SECTORNUMBER에 다시 설정
-    cmp al, byte [ LASTSECTOR ]         ; 증가시킨 섹터 번호를 마지막 섹터 번호와 비교
-    jle READDATA                        ; 섹터 번호가 마지막 섹터 이하라면 READDATA로 이동
+    cmp al, 19                          ; 증가시킨 섹터 번호를 19와 비교
+    jl READDATA                         ; 섹터 번호가 19 미만이라면 READDATA로 이동
     
-    ; 마지막 섹터까지 읽었으면 헤드를 증가시키고, 섹터 번호를 1로 설정
-    add byte [ HEADNUMBER ], 0x01       ; 헤드 번호를 1 증가
+    ; 마지막 섹터까지 읽었으면(섹터 번호가 19이면) 헤드를 토글(0->1, 1->0)하고, 
+    ; 섹터 번호를 1로 설정
+    xor byte [ HEADNUMBER ], 0x01       ; 헤드 번호를 0x01과 XOR하여 토글(0->1, 1->1)
     mov byte [ SECTORNUMBER ], 0x01     ; 섹터 번호를 다시 1로 설정
     
-    ; 만약 헤드를 모두 읽었으면 트랙 번호를 1 증가
-    mov al, byte [ LASTHEAD ]           ; 마지막 헤드 번호를 AL 레지스터에 설정
-    cmp byte [ HEADNUMBER ], al         ; 헤드 번호를 마지막 헤드 번호와 비교하고
-    jg .ADDTRACK                        ; 마지막 헤드 번호보다 크면 트랙 번호를 1 증가
-
-    jmp READDATA
-
-.ADDTRACK:
+    ; 만약 헤드가 1->0로 바뀌었으면 양쪽 헤드를 모두 읽은 것이므로 아래로 이동하여
+    ; 트랙 번호를 1 증가
+    cmp byte [ HEADNUMBER ], 0x00       ; 헤드 번호를 0x00과 비교
+    jne READDATA                        ; 헤드 번호가 0이 아니면 READDATA로 이동
+    
     ; 트랙을 1 증가시킨 후, 다시 섹터 읽기로 이동
-    mov byte [ HEADNUMBER ], 0x00       ; 헤드 번호를 0으로 설정
     add byte [ TRACKNUMBER ], 0x01      ; 트랙 번호를 1 증가
     jmp READDATA                        ; READDATA로 이동
-
 READEND:
 
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -181,51 +155,6 @@ READEND:
     call PRINTMESSAGE               ; PRINTMESSAGE 함수 호출
     add  sp, 6                      ; 삽입한 파라미터 제거
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; VBE 기능 번호 0x4F01을 호출하여 그래픽 모드에 대한 모드 정보 블록을 구함  
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    mov ax, 0x4F01      ; VBE 기능 번호 0x4F01를 AX 레지스터에 저장
-    mov cx, 0x117       ; 1024x768 해상도에 16비트(R(5):G(6):B(5)) 색 모드 지정
-    mov bx, 0x07E0      ; BX 레지스터에 0x07E0를 저장
-    mov es, bx          ; ES 세그먼트 레지스터에 BX의 값을 설정하고, DI 레지스터에 
-    mov di, 0x00        ; 0x00을 설정하여 0x07E0:0000(0x7E00) 어드레스에 모드 정보
-                        ; 블록을 저장
-    int 0x10            ; 인터럽트 서비스 수행
-    cmp ax, 0x004F      ; 에러가 발생했다면 VBEERROR로 이동
-    jne VBEERROR    
-
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; VBE 기능 번호 0x4F02을 호출하여 그래픽 모드로 전환 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; 부트 로더의 그래픽 모드 전환 플래그를 확인하여 1일 때만 그래픽 모드로 전환
-    cmp byte [ STARTGRAPHICMODE ], 0x00     ; 그래픽 모드 시작하는지 여부를 0x00과 비교
-    je JUMPTOPROTECTEDMODE                  ; 0x00과 같다면 바로 보호 모드로 전환
-    
-    mov ax, 0x4F02      ; VBE 기능 번호 0x4F02를 AX 레지스터에 저장
-    mov bx, 0x4117      ; 1024x768 해상도에 16비트(R(5):G(6):B(5)) 색을 사용하는 
-                        ; 선형 프레임 버퍼 모드 지정
-                        ; VBE 모드 번호(Bit 0~8) = 0x117, 
-                        ; 버퍼 모드(비트 14) = 1(선형 프레임 버퍼 모드)
-    int 0x10            ; 인터럽트 서비스 수행
-    cmp ax, 0x004F      ; 에러가 발생했다면 VBEERROR로 이동
-    jne VBEERROR    
-    
-    ; 그래픽 모드로 전환되었다면 보호 모드 커널로 이동
-    jmp JUMPTOPROTECTEDMODE
-
-VBEERROR:
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ;... 예외 처리 ...
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; 그래픽 모드 전환이 실패했다는 메시지를 출력
-    push CHANGEGRAPHICMODEFAIL
-    push 2
-    push 0
-    call PRINTMESSAGE
-    add  sp, 6    
-    jmp $
-
-JUMPTOPROTECTEDMODE:
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; 로딩한 가상 OS 이미지 실행    
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -280,26 +209,26 @@ PRINTMESSAGE:
     
     ; 출력할 문자열의 어드레스      
     mov si, word [ bp + 8 ]     ; 파라미터 3(출력할 문자열의 어드레스)
-    
+
 .MESSAGELOOP:               ; 메시지를 출력하는 루프
     mov cl, byte [ si ]     ; SI 레지스터가 가리키는 문자열 위치에서 한 문자를 
                             ; CL 레지스터에 복사
                             ; CL 레지스터는 CX 레지스터의 하위 1바이트를 의미
                             ; 문자열은 1바이트면 충분하므로 CX 레지스터의 하위 1바이트만 사용
-
+    
     cmp cl, 0               ; 복사된 문자와 0을 비교
     je .MESSAGEEND          ; 복사한 문자의 값이 0이면 문자열이 종료되었음을
                             ; 의미하므로 .MESSAGEEND로 이동하여 문자 출력 종료
 
     mov byte [ es: di ], cl ; 0이 아니라면 비디오 메모리 어드레스 0xB800:di에 문자를 출력
-
+    
     add si, 1               ; SI 레지스터에 1을 더하여 다음 문자열로 이동
     add di, 2               ; DI 레지스터에 2를 더하여 비디오 메모리의 다음 문자 위치로 이동
                             ; 비디오 메모리는 (문자, 속성)의 쌍으로 구성되므로 문자만 출력하려면
                             ; 2를 더해야 함
 
     jmp .MESSAGELOOP        ; 메시지 출력 루프로 이동하여 다음 문자를 출력
-    
+
 .MESSAGEEND:
     pop dx      ; 함수에서 사용이 끝난 DX 레지스터부터 ES 레지스터까지를 스택에
     pop cx      ; 삽입된 값을 이용해서 복원
@@ -313,30 +242,18 @@ PRINTMESSAGE:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;   데이터 영역
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; 부트 로더 시작 메시지는 심각한 오류를 나타내는 메시지를 제외하고 모두 삭제
-MESSAGE1:               db  0
+; 부트 로더 시작 메시지
+MESSAGE1:    db 'MINT64 OS Boot Loader Start~!!', 0 ; 출력할 메시지 정의
+                                                    ; 마지막은 0으로 설정하여 .MESSAGELOOP에서 
+                                                    ; 문자열이 종료되었음을 알 수 있도록 함
 DISKERRORMESSAGE:       db  'DISK Error~!!', 0
-IMAGELOADINGMESSAGE:    db  0
-LOADINGCOMPLETEMESSAGE: db  0
-CHANGEGRAPHICMODEFAIL:  db  0
-;MESSAGE1:    db 'MINT64 OS Boot Loader Start~!!', 0 ; 출력할 메시지 정의
-                                    ; 마지막은 0으로 설정하여 .MESSAGELOOP에서 
-                                    ; 문자열이 종료되었음을 알 수 있도록 함
-;DISKERRORMESSAGE:       db  'DISK Error~!!', 0
-;IMAGELOADINGMESSAGE:    db  'OS Image Loading...', 0
-;LOADINGCOMPLETEMESSAGE: db  'Complete~!!', 0
-;CHANGEGRAPHICMODEFAIL:  db  'Change Graphic Mode Fail~!!', 0
+IMAGELOADINGMESSAGE:    db  'OS Image Loading...', 0
+LOADINGCOMPLETEMESSAGE: db  'Complete~!!', 0
 
 ; 디스크 읽기에 관련된 변수들
 SECTORNUMBER:           db  0x02    ; OS 이미지가 시작하는 섹터 번호를 저장하는 영역
 HEADNUMBER:             db  0x00    ; OS 이미지가 시작하는 헤드 번호를 저장하는 영역
 TRACKNUMBER:            db  0x00    ; OS 이미지가 시작하는 트랙 번호를 저장하는 영역
-
-; 디스크 파라미터에 관련된 변수들
-BOOTDRIVE:              db 0x00     ; 부팅한 드라이브의 번호를 저장하는 영역
-LASTSECTOR:             db 0x00     ; 드라이브의 마지막 섹터 번호 -1을 저장하는 영역
-LASTHEAD:               db 0x00     ; 드라이브의 마지막 헤드 번호를 저장하는 영역
-LASTTRACK:              db 0x00     ; 드라이브의 마지막 트랙 번호를 저장하는 영역
     
 times 510 - ( $ - $$ )    db    0x00    ; $ : 현재 라인의 어드레스
                                         ; $$ : 현재 섹션(.text)의 시작 어드레스
--- 01.Kernel32/Source/EntryPoint.s
+++ 01.Kernel32/Source/EntryPoint.s
@@ -18,58 +18,21 @@ START:
     mov ds, ax      ; DS 세그먼트 레지스터에 설정
     mov es, ax      ; ES 세그먼트 레지스터에 설정
     
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; Application Processor 이면 아래의 과정을 모두 뛰어넘어서 보호 모드 커널로
-    ; 이동
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    mov ax, 0x0000              ; Application Processor 플래그를 확인하려고
-    mov es, ax                  ; ES 세그먼트 레지스터 의 시작 어드레스를 0으로 설정
-    
-    cmp byte [ es: 0x7C09 ], 0x00       ; 플래그가 0이면 Application Processor이므로
-    je .APPLICATIONPROCESSORSTARTPOINT  ; Application Processor용 코드로 이동
-    
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; Bootstrap Processor만 실행하는 부분
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; A20 게이트를 활성화
-    ; BIOS를 이용한 전환이 실패했을 때 시스템 컨트롤 포트로 전환 시도
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; BIOS 서비스를 사용해서 A20 게이트를 활성화
-    mov ax, 0x2401          ; A20 게이트 활성화 서비스 설정
-    int 0x15                ; BIOS 인터럽트 서비스 호출
-
-    jc .A20GATEERROR        ; A20 게이트 활성화가 성공했는지 확인
-    jmp .A20GATESUCCESS
-
-.A20GATEERROR:
-    ; 에러 발생 시, 시스템 컨트롤 포트로 전환 시도
-    in al, 0x92     ; 시스템 컨트롤 포트(0x92)에서 1 바이트를 읽어 AL 레지스터에 저장
-    or al, 0x02     ; 읽은 값에 A20 게이트 비트(비트 1)를 1로 설정
-    and al, 0xFE    ; 시스템 리셋 방지를 위해 0xFE와 AND 연산하여 비트 0를 0으로 설정
-    out 0x92, al    ; 시스템 컨트롤 포트(0x92)에 변경된 값을 1 바이트 설정
-    
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ; Bootstrap Processor와 Application Processor가 공통으로 수행하는 부분
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-.A20GATESUCCESS:
-.APPLICATIONPROCESSORSTARTPOINT:
     cli             ; 인터럽트가 발생하지 못하도록 설정
     lgdt [ GDTR ]   ; GDTR 자료구조를 프로세서에 설정하여 GDT 테이블을 로드
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ; 보호모드로 진입
     ; Disable Paging, Disable Cache, Internal FPU, Disable Align Check, 
     ; Enable ProtectedMode
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     mov eax, 0x4000003B ; PG=0, CD=1, NW=0, AM=0, WP=0, NE=1, ET=1, TS=1, EM=0, MP=1, PE=1
     mov cr0, eax        ; CR0 컨트롤 레지스터에 위에서 저장한 플래그를 설정하여 
                         ; 보호 모드로 전환
 
     ; 커널 코드 세그먼트를 0x00을 기준으로 하는 것으로 교체하고 EIP의 값을 0x00을 기준으로 재설정
     ; CS 세그먼트 셀렉터 : EIP
-    jmp dword 0x18: ( PROTECTEDMODE - $$ + 0x10000 )
+    jmp dword 0x08: ( PROTECTEDMODE - $$ + 0x10000 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
@@ -78,7 +41,7 @@ START:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 [BITS 32]               ; 이하의 코드는 32비트 코드로 설정
 PROTECTEDMODE:
-    mov ax, 0x20        ; 보호 모드 커널용 데이터 세그먼트 디스크립터를 AX 레지스터에 저장
+    mov ax, 0x10        ; 보호 모드 커널용 데이터 세그먼트 디스크립터를 AX 레지스터에 저장
     mov ds, ax          ; DS 세그먼트 셀렉터에 설정
     mov es, ax          ; ES 세그먼트 셀렉터에 설정
     mov fs, ax          ; FS 세그먼트 셀렉터에 설정
@@ -88,21 +51,15 @@ PROTECTEDMODE:
     mov ss, ax          ; SS 세그먼트 셀렉터에 설정
     mov esp, 0xFFFE     ; ESP 레지스터의 어드레스를 0xFFFE로 설정
     mov ebp, 0xFFFE     ; EBP 레지스터의 어드레스를 0xFFFE로 설정
-
-    ; Application Processor이면 아래의 과정을 모두 뛰어넘어서 C 언어 커널 엔트리
-    ; 포인트로 이동
-    cmp byte [ 0x7C09 ], 0x00
-    je .APPLICATIONPROCESSORSTARTPOINT    
     
-    ; 화면에 보호 모드로 전환되었다는 메시지를 표시
-    push ( SWITCHSUCCESSMESSAGE - $$ + 0x10000 )    ; 출력할 메시지의 어드레스를 스택에 삽입
+    ; 화면에 보호 모드로 전환되었다는 메시지를 찍는다.
+    push ( SWITCHSUCCESSMESSAGE - $$ + 0x10000 )    ; 출력할 메시지의 어드레스르 스택에 삽입
     push 2                                          ; 화면 Y 좌표(2)를 스택에 삽입
     push 0                                          ; 화면 X 좌표(0)를 스택에 삽입
     call PRINTMESSAGE                               ; PRINTMESSAGE 함수 호출
     add esp, 12                                     ; 삽입한 파라미터 제거
 
-.APPLICATIONPROCESSORSTARTPOINT:
-    jmp dword 0x18: 0x10200 ; C 언어 커널이 존재하는 0x10200 어드레스로 이동하여 C 언어 커널 수행
+    jmp $               ; 현재 위치에서 무한 루프 수행
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -155,7 +112,7 @@ PRINTMESSAGE:
     add edi, 2              ; EDI 레지스터에 2를 더하여 비디오 메모리의 다음 문자 위치로 이동
                             ; 비디오 메모리는 (문자, 속성)의 쌍으로 구성되므로 문자만 출력하려면
                             ; 2를 더해야 함
-    
+
     jmp .MESSAGELOOP        ; 메시지 출력 루프로 이동하여 다음 문자를 출력
 
 .MESSAGEEND:
@@ -173,8 +130,8 @@ PRINTMESSAGE:
 ; 아래의 데이터들을 8byte에 맞춰 정렬하기 위해 추가
 align 8, db 0
 
-    ; GDTR의 끝을 8byte로 정렬하기 위해 추가
-    dw 0x0000
+; GDTR의 끝을 8byte로 정렬하기 위해 추가
+dw 0x0000
 ; GDTR 자료구조 정의
 GDTR:
     dw GDTEND - GDT - 1         ; 아래에 위치하는 GDT 테이블의 전체 크기
@@ -191,24 +148,6 @@ GDT:
         db 0x00
         db 0x00
 
-    ; IA-32e 모드 커널용 코드 세그먼트 디스크립터
-    IA_32eCODEDESCRIPTOR:     
-        dw 0xFFFF       ; Limit [15:0]
-        dw 0x0000       ; Base [15:0]
-        db 0x00         ; Base [23:16]
-        db 0x9A         ; P=1, DPL=0, Code Segment, Execute/Read
-        db 0xAF         ; G=1, D=0, L=1, Limit[19:16]
-        db 0x00         ; Base [31:24]  
-        
-    ; IA-32e 모드 커널용 데이터 세그먼트 디스크립터
-    IA_32eDATADESCRIPTOR:
-        dw 0xFFFF       ; Limit [15:0]
-        dw 0x0000       ; Base [15:0]
-        db 0x00         ; Base [23:16]
-        db 0x92         ; P=1, DPL=0, Data Segment, Read/Write
-        db 0xAF         ; G=1, D=0, L=1, Limit[19:16]
-        db 0x00         ; Base [31:24]
-        
     ; 보호 모드 커널용 코드 세그먼트 디스크립터
     CODEDESCRIPTOR:     
         dw 0xFFFF       ; Limit [15:0]
@@ -226,7 +165,6 @@ GDT:
         db 0x92         ; P=1, DPL=0, Data Segment, Read/Write
         db 0xCF         ; G=1, D=1, L=0, Limit[19:16]
         db 0x00         ; Base [31:24]
-
 GDTEND:
 
 ; 보호 모드로 전환되었다는 메시지
